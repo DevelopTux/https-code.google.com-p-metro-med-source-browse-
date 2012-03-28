ef27ab2d82d463414950abee0dc25d25ee878e97
carlosgranada@gmail.com <carlosgranada@gmail.com@65a2c7fa-4028-3455-542e-f6688c6683c0>
2012-03-28 22:12:18 +0000

    Commit inicial de fuentes

---
+++ MetroMed/src/co/droidforum/metromed/MetroMedActivity.java
@@ -0,0 +1,13 @@
+package co.droidforum.metromed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MetroMedActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
