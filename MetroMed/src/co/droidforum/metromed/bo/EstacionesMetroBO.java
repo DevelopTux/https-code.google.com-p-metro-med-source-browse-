6378115516401c98e7ed0e1cea4d6e0230fdd5b5
cdmunoz@gmail.com <cdmunoz@gmail.com@65a2c7fa-4028-3455-542e-f6688c6683c0>
2012-04-04 16:14:32 +0000

    Se separa la logica de localizacion de la actividad y se pone en el BO

--- MetroMed/src/co/droidforum/metromed/bo/EstacionesMetroBO.java
+++ MetroMed/src/co/droidforum/metromed/bo/EstacionesMetroBO.java
@@ -10,11 +10,20 @@ import javax.inject.Inject;
 
 import android.content.ContentValues;
 import android.content.res.Resources.NotFoundException;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 import co.droidforum.metromed.R;
 import co.droidforum.metromed.application.AplicationContext;
 import co.droidforum.metromed.application.BusinessObject;
 import co.droidforum.metromed.dao.EstacionesDAO;
 import co.droidforum.metromed.dto.EstacionMetroDTO;
+
+import com.google.android.maps.GeoPoint;
 
 
 /**
@@ -97,4 +106,70 @@ public class EstacionesMetroBO {
 		    System.err.println("Failed to open microlog property file");
 		}
 	}
+	
+	
+	public GeoPoint obtenerLocalizacion(LocationManager locManager){
+		
+		GeoPoint geoPoint = null;
+		String bestProvider = "";
+		final LocationManager locManagerTmp = locManager;
+		LocationListener locListener = null;
+		
+		List<String> providers = locManager.getAllProviders();
+		for (String provider : providers) {
+			Log.e("-------------- Providers: ", provider);
+		}
+		
+		Criteria criteria = new Criteria();
+		bestProvider = locManager.getBestProvider(criteria, false);
+		final String bestProviderTmp = bestProvider;
+		
+		Location location = locManager.getLastKnownLocation(bestProvider);
+		Log.e("-------------- location: ", location!=null?location.toString():"Sin locacion");
+		
+		if(location != null){
+			geoPoint = getPosicionActual(location);
+		}
+		
+		
+    	//Se crea un listener que hace las llamadas necesarias en callback 
+    	//Nos registramos para recibir actualizaciones de la posición
+    	locListener = new LocationListener() {
+	    	public void onLocationChanged(Location location) {
+	    		locManagerTmp.requestLocationUpdates(bestProviderTmp, 20000, 1, this);
+	    		getPosicionActual(location);
+	    		locManagerTmp.removeUpdates(this);
+	    	}
+	    	public void onProviderDisabled(String provider){}
+	    	public void onProviderEnabled(String provider){}
+	    	public void onStatusChanged(String provider, int statusProvider, Bundle extras){}
+    	};
+    	/*
+    	 * Registra el listener en el Location Manager para recibir actualizaciones de locacion de las redes
+    	 * Los parametros 2 y 3 de requestLocationUpdates estan en 0 para indicar que nos de la posicion en vivo
+    	 * con el minimo intervalo de tiempo entre notificaciones.
+    	 * Al usar localizacion por Redes, debe agregarse el permiso android.permission.ACCESS_COARSE_LOCATION
+    	 * en el AndroidManifest.
+    	 */
+    	locManager.requestLocationUpdates(bestProvider, 20000, 1, locListener);
+    	locManager.removeUpdates(locListener);
+    	
+    	return geoPoint;
+	}
+	
+	/*
+	 * Este metodo lo que hace es setear una ubicacion basada en el objeto Location que puede tener informacion de alguna posicion anterior
+	 * o la nueva posicion obtenida. El objeto Location tiene en sus atributos la longitud y la latitud, que sirven como parametros para
+	 * la ubicacion que se hace con el objeto GeoPoint
+	 */
+	private GeoPoint getPosicionActual(Location location) {
+    	GeoPoint geoPoint = null;
+		if(location != null)
+    	{
+    		Toast.makeText(AplicationContext.getContextApp(), AplicationContext.getValueStringResource(R.string.msg_toast_carga_mapa), Toast.LENGTH_LONG).show();
+    		geoPoint = new GeoPoint(new Double(location.getLatitude()*1E6).intValue(), new Double(location.getLongitude()*1E6).intValue());
+    		
+    	}
+		return geoPoint;
+    }
 }
